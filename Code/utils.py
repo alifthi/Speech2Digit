4291cdb88bd9cf5806ae87346fb673d8512fd112
alifthi <alifathi8008@gmail.com>
2022-08-25 13:44:28 +0430

    Speech2Digit: in last format, addresses based on windows format i have change it!

--- Code/utils.py
+++ Code/utils.py
@@ -1,31 +1,37 @@
 import glob
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from tensorflow import keras as ks
 import numpy as np
 ##########################################
 class utils():
+    @staticmethod
     def readMnist():
         (trainIm, trainLabel), (testIm, testLabel) = ks.datasets.mnist.load_data()
-        trainIm = tf.convert_to_tensor(trainIm)/255.0
-        testIm = tf.convert_to_tensor(testIm)/255.0
+        trainIm = tf.convert_to_tensor(trainIm,dtype=tf.float32)/255.0
+        testIm = tf.convert_to_tensor(testIm,dtype=tf.float32)/255.0
         return [trainIm,trainLabel,testIm,testLabel]
+    @staticmethod
     def ReadAudio(path):
         Audio = []
         Labels = []
-        for i,file in enumerate(glob.glob(path+"\*.wav")):
+        for i,file in enumerate(glob.glob(path+"/*.wav")):
             audio = tf.io.read_file(file)
             waveForm,_ = tf.audio.decode_wav(contents=audio)
             waveForm = tf.squeeze(waveForm, axis=-1)
             Audio.append(waveForm)
-            Labels.append(file.split("\\")[-1].split("_")[0])
+            Labels.append(file.split("/")[-1].split("_")[0])
             if i%400 == 0:
                 print(f'{i}th file is readed')
         return Audio,Labels
+    @staticmethod
     def Audio2Spectrogram(Audio):
         Spectrogram = []
         for file in Audio:
             Spectrogram.append(ks.preprocessing.sequence.Spectrogram(file))
         return Spectrogram
+    @staticmethod
     def PlotWaveForm(Audio,Label):
         from matplotlib import pyplot as plt
         rows = 3
@@ -41,6 +47,7 @@ class utils():
             ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
             label = Label[t]
             ax.set_title(label)
+    @staticmethod
     def Audio2Spectrogram(Audio):
         InputLen = 16000
         Audio = Audio[:InputLen]
@@ -53,3 +60,6 @@ class utils():
         spectrogram = tf.abs(spectrogram)
         spectrogram = spectrogram[..., tf.newaxis]
         return spectrogram
+    @staticmethod
+    def audImagePair():
+        pass
